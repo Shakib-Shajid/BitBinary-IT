4f5f36746bfdcfecacd1bd914ecc8ef02f7f2655
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-06-12 03:39:56 +0600

    favicon changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,23 +2,32 @@ import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import NewsLetter from "@/components/shared/NewsLetter";
+import Head from 'next/head';
 
 
 export const metadata = {
   title: "Whale Lagoon",
   description: "It is a Social Media Marketing Agency",
+  icons:{
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className="bg-[#B8DCFF]">
         <div className="max-w-7xl mx-auto">
           <Navbar />
           {children}
           <NewsLetter />
         </div>
-          <Footer />
+        <Footer />
       </body>
     </html>
   );
