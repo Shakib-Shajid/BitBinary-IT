655662e989b06f239af3685eae0c0f6c6a7e1d81
shakib-shajid <shakibalshajid1682@gmail.com>
2024-10-30 19:52:57 +0600

    team page text resize

--- src/components/ui/Team.js
+++ src/components/ui/Team.js
@@ -8,7 +8,7 @@ const Team = () => {
             {/* Our Team Section */}
       <div className="hero min-h-screen">
         <div className="hero-content flex-col gap-20 lg:flex-row-reverse">
-          <div className="w-[50%] ">
+          <div className="w-[80%] lg:w-[50%] ">
             <h1 className="text-red-600">About Team</h1>
             <p className="text-5xl font-bold py-6 hidden md:flex">We Are The Leader <br />
               In Web Design</p>
@@ -16,12 +16,12 @@ const Team = () => {
             <p className="py-2 md:py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
             <p className="py-2 md:py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
             <Link href="/about">
-              <button className="btn bg-[#2C2CF2] text-white rounded-full md:py-4 md:px-10">Meet Our Team</button>
+              <button className="btn bg-[#2C2CF2] text-white rounded-full md:py-4 md:px-10 w-full md:w-1/3 border-none">Meet Our Team</button>
 
             </Link>
           </div>
           <Image
-            src={groupImg} className='md:w-2/4 lg:pt-6'
+            src={groupImg} className='w-3/4 md:w-3/4 lg:w-2/4 lg:pt-6'
             alt="Whale Lagoon" />
         </div>
       </div>
