dbe073b471cb5c4f950c5abab5e838b7581e2f4e
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-01 23:33:47 +0600

    navbar space added

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,13 +4,13 @@ import Blogs from '@/components/ui/Blogs';
 
 const page = () => {
     return (
-        <div>
+        <div data-aos="fade-right">
             <div className="text-center mt-3 md:mt-10">
                 <p className="text-red-600">Blogs</p>
                 <h3 className="text-3xl lg:text-5xl font-bold py-3">Our Latest Blogs</h3>
             </div>
 
-            <div>
+            <div >
                 <Blogs />
             </div>
         </div>
