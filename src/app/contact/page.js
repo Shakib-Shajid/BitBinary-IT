863e56eb7411b7cc5088a33e9b0b4ea72f662baa
shakib-shajid <shakibalshajid1682@gmail.com>
2025-03-12 13:21:40 +0600

    contact page responsive view added with updated design

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -9,7 +9,7 @@ const page = () => {
     return (
         <div>
            
-            <div className="flex gap-5 md:gap-20 flex-col lg:flex-row items-center">
+            <div className="flex gap-5 md:gap-10 lg:gap-20 flex-col lg:flex-row items-center py-12 md:py-16">
                 <div className="h-full bg-[#050591] rounded-xl text-white p-5 mt-5">
                     <h2 className="text-2xl md:text-4xl my-4 text-center lg:p-5">Let us Create Progress Together <br />
                         For Great Business</h2>
@@ -41,18 +41,18 @@ const page = () => {
                     </div>
                 </div>
 
-                <div>
-                    <p className="text-red-600  text-center">Contact With Us</p>
+                <div className="lg:pt-10">
+                    <p className="text-red-600 text-center">Contact With Us</p>
                     <h3 className="text-3xl lg:text-5xl font-bold py-3 text-center">Do not Google <br />
                         Design Questions</h3>
-                    <p className="md:my-2 lg:my-5 w-[80%] mx-auto text-center">Your email address will not be published. Required fields are marked* </p>
+                    <p className="md:my-2 lg:my-3 w-[80%] mx-auto text-center">Your email address will not be published. Required fields are marked* </p>
 
-                    <div className="my-5 md:my-10 w-[80%] md:w-[80%] lg:w-full mx-auto space-y-2 md:space-y-0">
+                    <div className="my-5 md:my-5 w-[80%] md:w-[80%] lg:w-full mx-auto space-y-2 md:space-y-0">
                         <div className="flex gap-2 md:gap-4 lg:gap-9 flex-col lg:flex-row space-y-2 md:space-y-0 mb-3">
-                            <input type="text" placeholder="Your Name*" className="input input-bordered w-full py-3 px-6 rounded-full text-sm h-16" required name="" id="" />
-                            <input type="email" placeholder="Your Email*" className="input input-bordered w-full py-3 px-6 rounded-full text-sm h-16" required name="" id="" />
+                            <input type="text" placeholder="Your Name*" className="input input-bordered w-full py-3 px-6 rounded-full text-sm h-12" required name="" id="" />
+                            <input type="email" placeholder="Your Email*" className="input input-bordered w-full py-3 px-6 rounded-full text-sm h-12" required name="" id="" />
                         </div>
-                        <textarea placeholder="Questions" className="textarea md:h-56 lg:h-80 w-full rounded-xl py-3 px-6 text-sm" ></textarea>
+                        <textarea placeholder="Questions" className="textarea md:h-56 lg:h-40 w-full rounded-xl py-3 px-6 text-sm" ></textarea>
                         <div className="flex justify-end">
                             <Link href="/contact" className="btn bg-[#2C2CF2] text-white mt-3 md:py-4 md:px-10 rounded-full border-0 w-full">Send Message</Link>
                         </div>
