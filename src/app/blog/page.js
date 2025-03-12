1fb7669cc7917acb73b378d4eac13baa1b9a5514
shakib-shajid <shakibalshajid1682@gmail.com>
2025-03-12 13:15:22 +0600

    blog section responsive view updated

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,7 +5,7 @@ import Blogs from '@/components/ui/Blogs';
 const page = () => {
     return (
         <div data-aos="fade-right">
-            <div className="text-center mt-3 md:mt-10">
+            <div className="text-center pt-[4.5rem]">
                 <p className="text-red-600">Blogs</p>
                 <h3 className="text-3xl lg:text-5xl font-bold py-3">Our Latest Blogs</h3>
             </div>
